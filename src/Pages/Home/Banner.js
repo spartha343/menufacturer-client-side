15dfd3a1ebe1623bfc6e2ac77ac238a9d1c25051
spartha343 <spartha343@gmail.com>
2022-05-28 23:25:27 +0600

    commit 4

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,6 +5,7 @@ const Banner = () => {
         <section>
             <div class="carousel w-full bg-base-100 shadow-xl">
                 <div id="item1" class="carousel-item w-full">
+
                     <img src=" https://i.ibb.co/rfc85QF/Trenchless-Best-1002-Allens-Repiper-1903x500-crop-center.webp" class="w-full" />
                 </div>
                 <div id="item2" class="carousel-item w-full">
